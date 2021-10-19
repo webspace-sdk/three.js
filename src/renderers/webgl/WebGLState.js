e8a41ea9fddc3a8ca0ee278bb8e567b84f56b649
netpro2k <netpro2k@gmail.com>
2021-10-18 18:28:33 -0700

    [HUBS] Fix "arguments" allocations in WebGLState
    WebGLState wraps texImage2D, compressedTexImage2D, and texImage3D to
    swallow exceptions. The upstream implementation uses "arguments" to
    handle the various function signatures. This can allocate in some
    javascript engines. Instead we hardcode the handful of signature
    patterns based on missing arguments. Note that we only need to account
    for the signatures with different arity, not type since things are
    directly passed through.
    
    This seems generally useful and we should try to land this upstream.

--- src/renderers/webgl/WebGLState.js
+++ src/renderers/webgl/WebGLState.js
@@ -858,11 +858,27 @@ function WebGLState( gl, extensions, capabilities ) {
 
 	}
 
-	function compressedTexImage2D() {
+	function compressedTexImage2D( a, b, c, d, e, f, g, h, i, j ) {
 
 		try {
 
-			gl.compressedTexImage2D.apply( gl, arguments );
+			if ( h === undefined ) {
+
+				gl.compressedTexImage2D( a, b, c, d, e, f, g );
+
+			} else if ( i === undefined ) {
+
+				gl.compressedTexImage2D( a, b, c, d, e, f, g, h );
+
+			} else if ( j === undefined ) {
+
+				gl.compressedTexImage2D( a, b, c, d, e, f, g, h, i );
+
+			} else {
+
+				gl.compressedTexImage2D( a, b, c, d, e, f, g, h, i, j );
+
+			}
 
 		} catch ( error ) {
 
@@ -872,11 +888,19 @@ function WebGLState( gl, extensions, capabilities ) {
 
 	}
 
-	function texImage2D() {
+	function texImage2D( a, b, c, d, e, f, g, h, i ) {
 
 		try {
 
-			gl.texImage2D.apply( gl, arguments );
+			if ( g === undefined ) {
+
+				gl.texImage2D( a, b, c, d, e, f );
+
+			} else {
+
+				gl.texImage2D( a, b, c, d, e, f, g, h, i );
+
+			}
 
 		} catch ( error ) {
 
@@ -886,11 +910,19 @@ function WebGLState( gl, extensions, capabilities ) {
 
 	}
 
-	function texImage3D() {
+	function texImage3D( a, b, c, d, e, f, g, h, i, j, k ) {
 
 		try {
 
-			gl.texImage3D.apply( gl, arguments );
+			if ( k === undefined ) {
+
+				gl.texImage3D( a, b, c, d, e, f, g, h, i, j );
+
+			} else {
+
+				gl.texImage3D( a, b, c, d, e, f, g, h, i, j, k );
+
+			}
 
 		} catch ( error ) {
 
